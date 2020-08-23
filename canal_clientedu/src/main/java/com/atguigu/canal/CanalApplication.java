015e8d821fce247b48c30b696e1fa3d2c70d9a2d
zhengwujia <418894252@qq.com>
2020-08-23 21:48:13 +0800

    创建Canal数据同步微服务模块

---
+++ canal_clientedu/src/main/java/com/atguigu/canal/CanalApplication.java
@@ -0,0 +1,31 @@
+package com.atguigu.canal;
+
+import com.atguigu.canal.client.CanalClient;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import javax.annotation.Resource;
+
+/**
+ * @author wu on 2020/8/23 0023
+ */
+
+
+@SpringBootApplication
+@ComponentScan("com.atguigu")
+public class CanalApplication implements CommandLineRunner {
+    @Resource
+    private CanalClient canalClient;
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanalApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        //项目启动自动执行监听器
+        canalClient.run();
+    }
+}
