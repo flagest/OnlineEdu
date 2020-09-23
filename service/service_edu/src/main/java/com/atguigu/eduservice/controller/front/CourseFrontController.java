fe11313bad26009d789b22e3ae4f4edbf47ebbc1
zhengwujia <418894252@qq.com>
2020-09-23 20:56:42 +0800

    新增前台根据id查询接口和新增修改接口

--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/front/CourseFrontController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/front/CourseFrontController.java
@@ -1,6 +1,7 @@
 package com.atguigu.eduservice.controller.front;
 
 import com.atguigu.commonutils.R;
+import com.atguigu.eduservice.entity.EduCourse;
 import com.atguigu.eduservice.mapper.EduCourseMapper;
 import com.atguigu.eduservice.service.EduChapterService;
 import com.atguigu.eduservice.service.EduCourseService;
@@ -8,6 +9,8 @@ import com.atguigu.eduservice.vo.ChapterVO;
 import com.atguigu.eduservice.vo.frontvo.CourseFrontVO;
 import com.atguigu.eduservice.vo.frontvo.CourseWebVO;
 import com.atguigu.servicebase.dto.CourseWebDTO;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.BeanUtils;
@@ -45,7 +48,7 @@ public class CourseFrontController {
     @GetMapping("getFrontCourseInfo/{courseId}")
     @ApiOperation(value = "更具课程id去查询课程详情方法")
     public R getFrontCourseInfo(@PathVariable String courseId, HttpServletRequest request) {
-        return eduCourseService.getCoursesInfo(courseId,request);
+        return eduCourseService.getCoursesInfo(courseId, request);
     }
 
     //根据课程id查询课程信息
@@ -56,5 +59,23 @@ public class CourseFrontController {
         CourseWebDTO courseWebDTO = new CourseWebDTO();
         BeanUtils.copyProperties(courseWebVO, courseWebDTO);
         return courseWebDTO;
+    }
+
+    //根据id查询
+    @GetMapping("/getById/{id}")
+    @ApiOperation(value = "根据id去查询")
+    public R getById(@PathVariable String id) {
+        LambdaQueryWrapper<EduCourse> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+        lambdaQueryWrapper.eq(EduCourse::getId, id);
+        return R.ok().data("ids", eduCourseService.getOne(lambdaQueryWrapper));
+    }
+
+    //新增或修改课程
+    @PostMapping("/save")
+    @ApiOperation(value = "新增或修改课程")
+    public R saveOrUpdate(@RequestBody EduCourse eduCourse) {
+        boolean b = eduCourseService.saveOrUpdate(eduCourse);
+        return b ? R.ok() : R.error();
+
     }
 }
